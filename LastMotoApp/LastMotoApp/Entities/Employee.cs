702a97dcf19d64b6a7daf8a29356cd3112527f58
Zetix7 <billyz7777777@gmail.com>
2024-11-22 15:13:49 +0100

    Create Employee.cs file

---
+++ LastMotoApp/LastMotoApp/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace LastMotoApp.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    public override string ToString() => $"Id: {Id}, Fullname: {FirstName} {LastName}";
+}
