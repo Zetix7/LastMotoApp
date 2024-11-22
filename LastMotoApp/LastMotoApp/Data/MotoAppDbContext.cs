8097ce111925f8bf9ec2886a0783760123c8d535
Zetix7 <billyz7777777@gmail.com>
2024-11-22 16:53:37 +0100

    Create BusinessPartner.cs and MotoAppDbContext.cs files

---
+++ LastMotoApp/LastMotoApp/Data/MotoAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LastMotoApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LastMotoApp.Data;
+
+public class MotoAppDbContext : DbContext
+{
+    public DbSet<Employee> Employees => Set<Employee>();
+    public DbSet<BusinessPartner> BusinessPartners => Set<BusinessPartner>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseInMemoryDatabase("MotoAppStorrage");
+    }
+}
