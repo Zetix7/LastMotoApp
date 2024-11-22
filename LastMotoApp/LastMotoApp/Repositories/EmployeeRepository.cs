8e0a98fdc3ec1f4c4d66bb299c77dc904558b496
Zetix7 <billyz7777777@gmail.com>
2024-11-22 15:21:51 +0100

    Create EmployeeRepository.cs file

---
+++ LastMotoApp/LastMotoApp/Repositories/EmployeeRepository.cs
@@ -0,0 +1,31 @@
+﻿using LastMotoApp.Entities;
+
+namespace LastMotoApp.Repositories;
+
+public class EmployeeRepository
+{
+    private List<Employee> _employees;
+
+    public EmployeeRepository()
+    {
+        _employees = new List<Employee>();
+    }
+
+    public Employee GetById(int id)
+    {
+        return _employees.Single(item => item.Id == id);
+    }
+
+    public void Add(Employee employee)
+    {
+        employee.Id = _employees.Count + 1;
+        _employees.Add(employee);
+    }
+
+    public void Save()
+    {
+        foreach (var employee in _employees)
+        {
+            Console.WriteLine(employee);
+        }
+}
