416f400f08d30baef0019782295e6677accf00df
Zetix7 <billyz7777777@gmail.com>
2024-11-22 17:06:04 +0100

    Implement SqlRepository class, use and display

--- LastMotoApp/LastMotoApp/Program.cs
+++ LastMotoApp/LastMotoApp/Program.cs
@@ -1,9 +1,22 @@
-﻿using LastMotoApp.Entities;
+﻿using LastMotoApp.Data;
+using LastMotoApp.Entities;
 using LastMotoApp.Repositories;
 
-var employeeRepository = new GenericRepository<Employee>();
-employeeRepository.Add(new Employee { FirstName = "Elizabeth", LastName = "Olsen" });
-employeeRepository.Add(new Employee { FirstName = "Scarlett", LastName = "Johansson" });
-employeeRepository.Add(new Employee { FirstName = "Ana", LastName = "de Armas" });
+//var employeeRepository = new GenericRepository<Employee>();
+//employeeRepository.Add(new Employee { FirstName = "Elizabeth", LastName = "Olsen" });
+//employeeRepository.Add(new Employee { FirstName = "Scarlett", LastName = "Johansson" });
+//employeeRepository.Add(new Employee { FirstName = "Ana", LastName = "de Armas" });
+//employeeRepository.Save();
 
-employeeRepository.Save();
+var sqlRepository = new SqlRepository(new MotoAppDbContext());
+sqlRepository.Add(new Employee { FirstName = "Elizabeth", LastName = "Olsen" });
+sqlRepository.Add(new Employee { FirstName = "Scarlett", LastName = "Johansson" });
+sqlRepository.Add(new Employee { FirstName = "Ana", LastName = "de Armas" });
+sqlRepository.Save();
+var e1 = sqlRepository.GetById(1);
+var e2 = sqlRepository.GetById(2);
+var e3 = sqlRepository.GetById(3);
+
+Console.WriteLine(e1);
+Console.WriteLine(e2);
+Console.WriteLine(e3);
---
+++ LastMotoApp/LastMotoApp/Repositories/SqlRepository.cs
@@ -0,0 +1,36 @@
+﻿using LastMotoApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LastMotoApp.Repositories;
+
+public class SqlRepository
+{
+    private readonly DbSet<Employee> _dbSet;
+    private readonly DbContext _dbContext;
+
+    public SqlRepository(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = _dbContext.Set<Employee>();
+    }
+
+    public Employee GetById(int id)
+    {
+        return _dbSet.Single(item => item.Id == id);
+    }
+
+    public void Add(Employee item)
+    {
+        _dbSet.Add(item);
+    }
+
+    public void Remove(Employee item)
+    {
+        _dbSet.Remove(item);
+    }
+
+    public void Save()
+    {
+        _dbContext.SaveChanges();
+    }
+}
